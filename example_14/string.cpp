8ffd94999f1ee2d000f9e30ea8b0935a842875a5
Emil Meng <emil.meng@gmail.com>
2021-01-27 16:30:43 -0700

    added some notes from lectures

--- example_14/string.cpp
+++ example_14/string.cpp
@@ -1,6 +1,14 @@
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
+#include <string>
+
+int atoi_practice(const char *num)
+{
+    std::cout << "In function, i have received: " << num << std::endl;
+    std::cout << "strlen of num is: " << strlen(num) << std::endl;
+    return 0;
+}
 
 int main (int argc, char **argv)
 {
@@ -74,6 +82,63 @@ int main (int argc, char **argv)
         char animal2[] = "fire";
         int result = strcmp(animal1, animal2);
 
+#if 0
+        1286981659613891899
+        1 digit - 10    -- 10**1
+        2 digit - 100   -- 10**2
+        3 digit - 1000  -- 10**3
+        4 digit - 10000 -- 10**4
+
+        1 binary digit, 0, 1              - 2
+        2 binary digit, 00, 01, 10, 11    - 4
+        3 binary digit, 00, 01, 10, 11... - 8
+        4 binary digit, 00, 01, 10, 11... - 16
+        5 binary digit, 00, 01, 10, 11... - 32
+        6 binary digit, 00, 01, 10, 11... - 64
+        7 binary digit, 00, 01, 10, 11... - 128
+        8 binary digit, 00, 01, 10, 11... - 256
+
+        abcdefghijklmnopqrstuvwxyz
+        ABCDEFGHIJKLMNOPQRSTUVWXYZ
+        1234567890
+        !@#$%^&*()
+
+        ASCII
+#endif
+
         std::cout << result << std::endl;
+    }
+    {
+        char foo[] = "Hello I am 27 years old";
+
+        std::cout << foo << std::endl;
+
+        //foo[11] = 51;
+        //foo[11] = 0x33;
+        foo[11] = '3';
+
+        std::cout << foo << std::endl;
+    }
+    {
+        char foo[] = "fire";
+        char foo2[] = "fira";
+
+
+        int difference = foo[3] - 'a';
+
+        std::cout << difference << std::endl;
+        std::cout << strcmp(foo2, foo) << std::endl;
+
+        char animal1[] = "aardvark";
+        char animal2[] = "zebra";
+
+        std::cout << strcmp(animal1, animal2) << std::endl;
+    }
+    {
+        std::string number;
+
+        std::cin >> number;
+        std::cout << "I got number to be: " << number << std::endl;
+        int num = atoi_practice(number.c_str());
     }
 }
