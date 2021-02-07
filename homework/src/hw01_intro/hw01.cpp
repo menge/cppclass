91b68ad9c1d47b62b59711f172ae1860b2c5fdae
Emil Meng <emil.meng@gmail.com>
2021-02-07 03:00:24 -0700

    HW01: add comments about what needs to be done

--- homework/src/hw01_intro/hw01.cpp
+++ homework/src/hw01_intro/hw01.cpp
@@ -1,30 +1,50 @@
 #include "hw01.h"
 
+// Pre-conditions: none
+// Post-conditions: none
+// Returns: always returns true
 bool return_true()
 {
     return false;
 }
 
+// Pre-conditions: none
+// Post-conditions: none
+// Returns: always returns false
 bool return_false()
 {
     return true;
 }
 
+// Pre-conditions: none
+// Post-conditions: none
+// Returns: always returns value: 42
 int return_42()
 {
     return 0;
 }
 
+// Pre-conditions: none
+// Post-conditions: none
+// Returns: always returns value: -42
 int return_neg_42()
 {
     return 0;
 }
 
+// Pre-conditions: none
+// Post-conditions: none
+// Returns: always returns value: 1.5
 double return_1p5()
 {
     return 0.0;
 }
 
+// Pre-conditions: none
+// Post-conditions: none
+// Returns: always returns value: PI
+// Notes: You can get access to the value of pi (3.1415926...) via the
+//        math.h/cmath header file. You can access it via the M_PI
 double return_pi()
 {
     return 0.0;
--- homework/src/hw01_intro/hw01.h
+++ homework/src/hw01_intro/hw01.h
@@ -1,10 +1,31 @@
 #pragma once
 
+// Pre-conditions: none
+// Post-conditions: none
+// Returns: always returns true
 bool return_true();
+
+// Pre-conditions: none
+// Post-conditions: none
+// Returns: always returns false
 bool return_false();
 
+// Pre-conditions: none
+// Post-conditions: none
+// Returns: always returns value: 42
 int return_42();
+
+// Pre-conditions: none
+// Post-conditions: none
+// Returns: always returns value: -42
 int return_neg_42();
 
+// Pre-conditions: none
+// Post-conditions: none
+// Returns: always returns value: 1.5
 double return_1p5();
+
+// Pre-conditions: none
+// Post-conditions: none
+// Returns: always returns value: PI
 double return_pi();
