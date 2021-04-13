79df36cf9e758dea40c4f5462559fa916116f339
Emil Meng <emil.meng@gmail.com>
2021-04-13 17:42:55 -0600

    lecture notes: struct alignment

--- example_17/struct.cpp
+++ example_17/struct.cpp
@@ -13,6 +13,27 @@ struct FishTankv2 {
     int numFish;
 };
 
+struct UserInterface {
+    char theme[40];
+    bool darkInterface;
+    int pageVists;
+    bool searchBarExists;
+    double costOfPage;
+};
+
+struct PlayStructv1 {
+    char b[1];
+    double a[2];
+};
+PlayStructv1 myStructs[10];
+
+struct PlayStruct {
+    double a[2];
+    char b[1];
+    int c;
+    char d[1];
+};
+
 int main(int argc, char** argv)
 {
 #if 0
@@ -36,4 +57,15 @@ int main(int argc, char** argv)
     std::cout << "name: " << newWaterWaster.name << std::endl;
     std::cout << "capacity: " << newWaterWaster.capacity << std::endl;
     std::cout << "numFish: " << newWaterWaster.numFish << std::endl;
+
+    UserInterface ui;
+    std::cout << "sizeof theme: " << sizeof(ui.theme) << std::endl;
+    std::cout << "sizeof darkInterface: " << sizeof(ui.darkInterface) << std::endl;
+    std::cout << "sizeof pageVists: " << sizeof(ui.pageVists) << std::endl;
+    std::cout << "sizeof searchBarExists: " << sizeof(ui.searchBarExists) << std::endl;
+    std::cout << "sizeof costOfPage: " << sizeof(ui.costOfPage) << std::endl;
+    std::cout << "sizeof UserInterface: " << sizeof(ui) << std::endl;
+
+    PlayStruct ps;
+    std::cout << "sizeof PlayStruct: " << sizeof(ps) << std::endl;
 }
